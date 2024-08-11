2b1c3ae5d4035f6d34495748bb58a251a1614d03
hack <diaodabijin@gmail.com>
2024-08-11 17:16:26 +0800

    add forwardlistfind1

---
+++ chapter07/forwardlistsplice1/print.hpp
@@ -0,0 +1,17 @@
+#ifndef PRINT_HPP
+#define PRINT_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+inline void PRINT_ELEMENTS(const T& coll, const std::string &optstr=""){
+    using namespace std;
+    cout << optstr;
+    for(const auto &elem : coll){
+        cout << elem << ' ';
+    }
+    cout << endl;
+}
+
+#endif // PRINT_HPP
